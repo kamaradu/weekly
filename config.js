183241822fee98247856e9027db186f96f3dbdaa
kamaradu <bezruk10@gmail.com>
2026-08-31 23:20:07 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+/*
+  НАЛАШТУВАННЯ
+  1. Створіть/опублікуйте аркуш SiteData у Google Sheets.
+  2. Скопіюйте URL CSV експорту цього аркуша нижче.
+  3. SiteData не потребує week_start/week_end: тиждень визначається сайтом за календарем Пн–Нд.
+  3. Якщо URL порожній, сайт використовує demo-data.json.
+*/
+window.SITE_CONFIG = {
+  CSV_URL: '',
+  FALLBACK_DATA_URL: './demo-data.json',
+  TITLE: 'Статистика за тиждень',
+  LOCALE: 'uk-UA',
+  WEEK_STARTS_MONDAY: true,
+  METRIC_COUNT: 7
+};
